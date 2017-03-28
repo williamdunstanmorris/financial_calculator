6e652d075f727648e689acf5d387a57454aaacbd
williammorris <wmorris421@gmail.com>
2017-03-28 12:26:35 +0100

    Draft of test case 1

---
+++ test/core/HP12CFunctionsTest.java
@@ -0,0 +1,24 @@
+package core;
+
+import org.junit.After;
+import org.junit.Before;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by williammorris on 27/03/2017.
+ */
+public class HP12CFunctionsTest {
+    @Before
+    public void setUp() throws Exception {
+
+    }
+
+    @After
+    public void tearDown() throws Exception {
+
+    }
+
+
+
+}
